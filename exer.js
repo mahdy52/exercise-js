672e0ee7dc6772f7fd980e083ca7d6713e8c8d11
mahdy52 <mahditlilisimplonenda@gmail.com>
2021-03-30 17:53:00 +0100

    excercice js

---
+++ exer.js
@@ -0,0 +1,38 @@
+//*********ex1****** */
+function permut(a,b){ 
+    let ch;
+    ch=a;
+    a=b;
+    b=ch;
+    return "valeur de a " + a + " valeur de b " + b
+}
+
+console.log(permut(2,3))
+//********ex2****** */
+function appl(){
+    let Nom="toto"
+    let age="30"
+    let homme= true 
+
+    return 'your name is ' +Nom+' your age is ' +age+ ' years ' +homme
+
+}
+console.log(appl());
+//*********ex3*** */
+function fois(a){
+    let p=a*a;
+    return p
+}
+console.log(fois(5))
+//********ex4**** */
+function somm(a,b){
+let so =a+b
+    return so
+}
+console.log(somm(5,25))
+//**********ex4***** */
+function time(h){
+let so =h*60;
+    return so
+}
+console.log (time(10))
